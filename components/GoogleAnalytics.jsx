4278aafe10af266f367fb9e2dd7b9733cd575e50
369webforall <369webforall@gmail.com>
2023-09-25 21:50:15 +0545

    analytics

--- components/GoogleAnalytics.jsx
+++ components/GoogleAnalytics.jsx
@@ -2,15 +2,19 @@
 
 import React from 'react';
 import Script from 'next/script';
-const id = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS
+const id = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 const GoogleAnalytics = () => {
   return (
-        <>
-            <Script strategy="afterInteractive" 
-                src={`https://www.googletagmanager.com/gtag/js?id=${id}`}/>
-            <Script id='google-analytics' strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                __html: `
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${id}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
@@ -23,9 +27,10 @@ const GoogleAnalytics = () => {
                     page_path: window.location.pathname,
                 });
                 `,
-                }}
-            />
-        </>
+        }}
+      />
+    </>
+  );
 };
 
 export default GoogleAnalytics;
