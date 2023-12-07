fdc972dc39b6860f27d78a76fdf5a46fbeddea3a
369webforall <369webforall@gmail.com>
2023-12-07 14:34:43 +0545

    update

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -114,10 +114,7 @@ const Blogs = () => {
               </li>
               <li>Gibt es spezifische Anforderungen an Soft Skills?</li>
               <li>Wie gestaltet sich der Auswahlprozess?</li>
-              <li>
-                Welche Unternehmen haben Interesse an Kandidaten mit meinen
-                Qualifikationen?
-              </li>
+
               <li>Wie lange dauert der Vermittlungsprozess im Durchschnitt?</li>
               <li>
                 Welche Unterstützung bieten Sie während des Bewerbungsprozesses?
