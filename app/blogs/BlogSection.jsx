83f20abb64abdae36a8c59d900e61d366962c823
369webforall <369webforall@gmail.com>
2023-12-12 13:22:27 +0545

    fixed two issue

--- app/blogs/BlogSection.jsx
+++ app/blogs/BlogSection.jsx
@@ -3,21 +3,21 @@ import Link from 'next/link';
 const BlogSection = () => {
   return (
     <div className="w-full">
-      <h1 className="text-xl md:text-2xl">Blogs</h1>
+      <h2 className="text-xl md:text-2xl">Blogs</h2>
       <div className="mt-6 border-b pb-6">
         <p className="text-sm italic font-sans"> 01-11-2023</p>
         <Link href="/blogs">
-          <h2 className="font-semibold md:text-xl font-sans">
+          <h3 className="font-semibold md:text-xl font-sans">
             Was macht eine Personalvermittlung?
-          </h2>
+          </h3>
         </Link>
       </div>
       <div className="mt-6 border-b pb-6">
         <p className="text-sm italic font-sans"> 04-12-2023</p>
-        <Link href="/blogs/first    ">
-          <h2 className="font-semibold md:text-xl font-sans">
+        <Link href="/blogs/first">
+          <h3 className="font-semibold md:text-xl font-sans">
             Meistern Sie Ihr Recruiting Meisterstück im Jahr 2024!
-          </h2>
+          </h3>
         </Link>
       </div>
     </div>
