b2d44b818d33aba4c0779ea98fd74e0b08c95d80
369webforall <369webforall@gmail.com>
2024-12-08 18:51:06 +0545

    remove employee

--- components/EmployeeSection.jsx
+++ components/EmployeeSection.jsx
@@ -3,9 +3,7 @@ import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
 
 import { employeeData } from "../constants/index";
-import { Facebook, Twitter, Linkedin } from 'lucide-react';
-
-
+import { Facebook, Twitter, Linkedin } from "lucide-react";
 
 const EmployeeCard = ({ name, jobTitle, imageUrl, task }) => {
   return (
@@ -18,15 +16,15 @@ const EmployeeCard = ({ name, jobTitle, imageUrl, task }) => {
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
           {name}
         </h5>
-        
+
         <p className="mb-3 font-normal text-gray-700 text-lg">{jobTitle}</p>
         <p className="text-lg text-slate-800 mb-4">Aufgaben:</p>
         <p className="text-gray-600 text-md mb-4">{task}</p>
       </div>
       <div className="flex items-center justify-center gap-4">
-<Facebook className="text-orange-800" />
-<Twitter className="text-orange-800"/>
-<Linkedin className="text-orange-800"/>
+        <Facebook className="text-orange-800" />
+        <Twitter className="text-orange-800" />
+        <Linkedin className="text-orange-800" />
       </div>
     </div>
   );
@@ -36,20 +34,20 @@ const EmployeeSection = () => {
   return (
     <div className="px-4 py-6 md:py-12 bg-orange-400 rounded-sm">
       <div className="container mx-auto">
-      <h2 className="text-lg md:text-3xl font-thin mb-8 text-center font-roboto text-slate-700">
-        Unser Team setzt Ihre Wünsche professionell und zeitnah um
-      </h2>
-      <div className="max-w-[1000px] mx-auto grid grid-cols-1 md:grid-cols-3 gap-16">
-        {employeeData.map((employee) => (
-          <EmployeeCard
-            key={employee.id}
-            name={employee.name}
-            jobTitle={employee.title}
-            imageUrl={employee.img}
-            task={employee.task}
-          />
-        ))}
-      </div>
+        <h2 className="text-lg md:text-3xl font-thin mb-8 text-center font-roboto text-slate-700">
+          Unser Team setzt Ihre Wünsche professionell und zeitnah um
+        </h2>
+        <div className="max-w-[400px] mx-auto grid grid-cols-1 gap-16">
+          {employeeData.map((employee) => (
+            <EmployeeCard
+              key={employee.id}
+              name={employee.name}
+              jobTitle={employee.title}
+              imageUrl={employee.img}
+              task={employee.task}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- constants/index.js
+++ constants/index.js
@@ -1,106 +1,91 @@
 export const navData = [
-  { _id: 101, title: 'Home', href: '/' },
-  { _id: 102, title: 'Jobs', href: '/jobs' },
-  { _id: 103, title: 'Blogs', href: '/blogs' },
-  { _id: 104, title: 'Kontakt', href: '/contact' },
+  { _id: 101, title: "Home", href: "/" },
+  { _id: 102, title: "Jobs", href: "/jobs" },
+  { _id: 103, title: "Blogs", href: "/blogs" },
+  { _id: 104, title: "Kontakt", href: "/contact" },
   // { _id: 105, title: 'Admin', href: '/login' },
 ];
 
 export const jobData = [
   {
     id: 1,
-    title: 'Fahrer/Zusteller mit B Führerschein',
-    company: 'LD-Personalvermittlungs',
+    title: "Fahrer/Zusteller mit B Führerschein",
+    company: "LD-Personalvermittlungs",
     description:
-      'Als Fahrer/Zusteller mit einem B Führerschein sind Sie ein wichtiger Teil unseres Liefer- und Zustellteams.',
-    details: 'JOB-DETAILS ANZEIGEN',
-    link: '/fahrer-zusteller',
+      "Als Fahrer/Zusteller mit einem B Führerschein sind Sie ein wichtiger Teil unseres Liefer- und Zustellteams.",
+    details: "JOB-DETAILS ANZEIGEN",
+    link: "/fahrer-zusteller",
   },
   {
     id: 2,
-    title: 'LKW Fahrer',
-    company: 'LD-Personalvermittlungs',
+    title: "LKW Fahrer",
+    company: "LD-Personalvermittlungs",
     description:
-      'Als LKW-Fahrer spielen Sie eine Schlüsselrolle in unserem Logistikteam.',
-    details: 'JOB-DETAILS ANZEIGEN',
-    link: '/lkw-fahrer',
+      "Als LKW-Fahrer spielen Sie eine Schlüsselrolle in unserem Logistikteam.",
+    details: "JOB-DETAILS ANZEIGEN",
+    link: "/lkw-fahrer",
   },
   {
     id: 3,
-    title: 'Produktionshelfer',
-    company: 'LD-Personalvermittlungs',
+    title: "Produktionshelfer",
+    company: "LD-Personalvermittlungs",
     description:
-      'Als Produktionshelfer sind Sie in unserer Fertigungsabteilung tätig und unterstützen bei verschiedenen Aufgaben im Produktionsprozess.',
-    details: 'JOB-DETAILS ANZEIGEN',
-    link: '/produktionshelfer',
+      "Als Produktionshelfer sind Sie in unserer Fertigungsabteilung tätig und unterstützen bei verschiedenen Aufgaben im Produktionsprozess.",
+    details: "JOB-DETAILS ANZEIGEN",
+    link: "/produktionshelfer",
   },
   {
     id: 4,
-    title: 'Lagerhelfer',
-    company: 'LD-Personalvermittlungs',
+    title: "Lagerhelfer",
+    company: "LD-Personalvermittlungs",
     description:
-      'Als Lagerhelfer sind Sie für die Organisation und Verwaltung unseres Lagers verantwortlich.',
-    details: 'JOB-DETAILS ANZEIGEN',
-    link: '/lagerhelfer',
+      "Als Lagerhelfer sind Sie für die Organisation und Verwaltung unseres Lagers verantwortlich.",
+    details: "JOB-DETAILS ANZEIGEN",
+    link: "/lagerhelfer",
   },
   {
     id: 5,
-    title: 'Lagermitarbeiter',
-    company: 'LD-Personalvermittlungs',
+    title: "Lagermitarbeiter",
+    company: "LD-Personalvermittlungs",
     description:
-      'Lagermitarbeiter/Staplerfahrer/Kommissionierer (m/w/d)- Deutschlandweit.',
-    details: 'JOB-DETAILS ANZEIGEN',
-    link: '/lagermitarbeiter',
+      "Lagermitarbeiter/Staplerfahrer/Kommissionierer (m/w/d)- Deutschlandweit.",
+    details: "JOB-DETAILS ANZEIGEN",
+    link: "/lagermitarbeiter",
   },
   {
     id: 6,
-    title: 'Paketzusteller',
-    company: 'LD-Personalvermittlungs',
+    title: "Paketzusteller",
+    company: "LD-Personalvermittlungs",
     description:
-      ' Paketzusteller (m/w/d) bei dem größten Logistikdienstleister Deutschlands  in München.',
-    details: 'JOB-DETAILS ANZEIGEN',
-    link: '/paketzusteller',
+      " Paketzusteller (m/w/d) bei dem größten Logistikdienstleister Deutschlands  in München.",
+    details: "JOB-DETAILS ANZEIGEN",
+    link: "/paketzusteller",
   },
   {
     id: 7,
-    title: 'Paketzusteller',
-    company: 'LD-Personalvermittlungs',
+    title: "Paketzusteller",
+    company: "LD-Personalvermittlungs",
     description:
-      'Paketzusteller (m/w/d) bei einem namhaften Kunden in Steißlingen',
-    details: 'JOB-DETAILS ANZEIGEN',
-    link: '/paketzusteller1',
+      "Paketzusteller (m/w/d) bei einem namhaften Kunden in Steißlingen",
+    details: "JOB-DETAILS ANZEIGEN",
+    link: "/paketzusteller1",
   },
   {
     id: 8,
-    title: 'Recruiter',
-    company: 'LD-Personalvermittlungs',
-    description:
-      ' Recruiter (m/w/d) - Europaweit im Homeoffice',
-    details: 'JOB-DETAILS ANZEIGEN',
-    link: '/recruiter',
+    title: "Recruiter",
+    company: "LD-Personalvermittlungs",
+    description: " Recruiter (m/w/d) - Europaweit im Homeoffice",
+    details: "JOB-DETAILS ANZEIGEN",
+    link: "/recruiter",
   },
 ];
 
-export const employeeData = [{
-  id:1,
-  name:'Lily Loredana',
-  title:'Rekrutierungskoordinator',
-  img:'/employee/Lily.jpg',
-  task:'Organisation, Verwaltung, Mitarbeiter und Kundenbetreuung'
-},
-{
-  id:2,
-  name:'Izabell',
-  title:'Koordinator Rumänien',
-  img:'/employee/Izabell.jpg',
-  task:'Mitarbeiter- und Kundenbetreuung, Rekrutierungskoordinator für Mitarbeiter aus Rumänien'
-
-},
-{
-  id:3,
-  name:'Dennis Schwarz',
-  title:'Recruiter',
-  img:'/employee/Dennis.jpg',
-  task:'Rekrutierung von Mitarbeitern aus Rumänien und Deutschland'
-}
-]
+export const employeeData = [
+  {
+    id: 1,
+    name: "Lily Loredana",
+    title: "Rekrutierungskoordinator",
+    img: "/employee/Lily.jpg",
+    task: "Organisation, Verwaltung, Mitarbeiter und Kundenbetreuung",
+  },
+];
